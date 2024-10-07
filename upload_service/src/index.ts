2f0661e110b02393a05ab11db231f34f64904c5c
Dhruv Sood <dhruvsoodboss@gmail.com>
2024-10-07 21:18:10 +0530

    Add utility to get all files from cloned repo in array

--- upload_service/src/index.ts
+++ upload_service/src/index.ts
@@ -2,20 +2,26 @@ import express, { Request, Response } from "express";
 import { v4 as uuidv4 } from "uuid";
 import simpleGit from "simple-git";
 
+import { getAllFiles } from "./utils";
 
+import path from "path";
 
 const app = express();
 
 app.use(express.json());
 
-app.post("/deploy", async (req, res) => {
+app.post("/deploy", async (req: Request, res: Response) => {
   const repoUrl = req.body.repoUrl;
   const id = uuidv4().slice(0, 5);
 
-  await simpleGit().clone(repoUrl, `./${id}`)
+  await simpleGit().clone(repoUrl, path.join(__dirname, `../output/${id}`));
+  const files = getAllFiles(`./output/${id}`);
   res.json({
-    id 
+    id,
+    files
   })
+
+  
 
 });
 
